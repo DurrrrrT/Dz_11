7f6d0f690ac7fb441566f196dd95ab1256b0ba00
Amir <amir200320032003@gmail.com>
2022-05-03 21:41:00 +0300

    first commit

---
+++ Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int[] info = {0, 1, 1, 1, 1, 0, 0, 0, 1 };
+int[] data = { 2, 3, 3, 1 };
+
+int index = 0;
+string numberAsString = string.Empty;
+
+foreach (int value in data) 
+{
+    numberAsString = ""; 
+
+    for (int i = 0; i < value; i++) 
+    {
+        numberAsString += info[index]; 
+        index++; 
+    }
+
+int newNumber = Convert.ToInt32(numberAsString, 2);
+Console.Write(newNumber + " ");
+}
